0a956117c7e1213136e3ff69aa174de607b7f764
kyamisuke <murakami.k.am@m.titech.ac.jp>
2023-09-02 13:32:21 +0900

    Initial Commit

---
+++ numajya/numajyaApp.swift
@@ -0,0 +1,17 @@
+//
+//  numajyaApp.swift
+//  numajya
+//
+//  Created by 村上航輔 on 2023/09/02.
+//
+
+import SwiftUI
+
+@main
+struct numajyaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
